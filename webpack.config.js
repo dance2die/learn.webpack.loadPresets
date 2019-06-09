2d306d4a165e5ee6816277e06392207a46842cd8
Sung M. Kim <sungmkim80@gmail.com>
2019-06-09 17:32:06 -0400

    Enabled webpack progress plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = ({ mode }) => ({
   mode,
   output: {
     path: path.join(__dirname, "./dist"),
     filename: "[chunkhash].js"
-  }
+  },
+  plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
 });
